285ee246b4258ac5f9a7d25214bb1bbc2176823a
hriks <hriks@outlook.com>
2019-03-18 12:03:15 +0530

    Added Admins for user module

--- users/admin.py
+++ users/admin.py
@@ -5,7 +5,9 @@ from django.contrib import admin
 
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from users.models import Account, User
+from users.models import (
+    Account, User, Campaign, UserDetails, Documents, Bank, BankAccount,
+    BankBranch)
 
 
 @admin.register(User)
@@ -34,3 +36,47 @@ class AccountAdmin(admin.ModelAdmin):
     list_display = ('phone_no', 'pan_no', 'gender')
     list_filter = ('gender', 'pincode')
     search_fields = ('phone_no', 'alternate_no', 'aadhar_no')
+
+
+@admin.register(UserDetails)
+class UserDetailsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'agent_code', 'channel', 'status')
+    list_filter = ('channel', 'status')
+    search_fields = ('user__username', 'user__account__phone_no')
+    raw_id_fields = ('user')
+
+
+@admin.register(Documents)
+class DocumentsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'doc_type', 'file')
+    search_fields = ('user__username', 'user__account__phone_no')
+    raw_id_fields = ('user')
+    list_filter = ('doc_type',)
+
+
+@admin.register(Bank)
+class BankAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active')
+    list_filter = ('name', 'is_active')
+
+
+@admin.register(BankAccount)
+class BankAccountAdmin(admin.ModelAdmin):
+    list_display = ('user', 'branch', 'default')
+    search_fields = ('user__username', 'user__account__phone_no')
+    raw_id_fields = ('user', 'branch')
+    list_filter = ('default',)
+
+
+@admin.register(BankBranch)
+class BankBranchAdmin(admin.ModelAdmin):
+    list_display = ('bank', 'branch_name', 'ifsc', 'city', 'micr')
+    search_fields = (
+        'branch_name__user__username', 'branch_name__user__account__phone_no')
+    raw_id_fields = ('bank', 'branch_name')
+    list_filter = ('default',)
+
+
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = ('description',)
