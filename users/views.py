574ffd781ca43be2d425682ef507c568ad6b3062
samim <samim@goplannr.com>
2019-04-03 12:16:40 +0530

    Added advisor search api.

--- users/views.py
+++ users/views.py
@@ -2,13 +2,17 @@ from __future__ import unicode_literals
 from rest_framework import permissions, status, generics
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.views import APIView
+from django.db.models import Q
 
 from url_filter.integrations.drf import DjangoFilterBackend
 
 from users.serializers import (
     CreateUserSerializer, OTPGenrationSerializer, OTPVerificationSerializer,
     AuthorizationSerializer, ChangePasswordSerializer,
-    AccountSearchSerializers, Account, PincodeSerializer, Pincode
+    AccountSearchSerializers, Account, User, PincodeSerializer, Pincode,
+    UserSerializer
 )
 
 
@@ -60,22 +64,103 @@ class SearchAccount(generics.ListAPIView):
     serializer_class = AccountSearchSerializers
 
     def get_queryset(self):
-        queryset = Account.objects.filter(
-            user__user_type__in=['subscriber', 'pos'])
+
+        query = {}
+        filter_location = {}
         params = self.request.query_params
-        if 'pincode' in params:
-            queryset = queryset.filter(pincode=params['pincode'])
-        if 'company' in params:
-            queryset = queryset.filter(
-                user__enterprise__company_id=params['company'])
-        if 'category' in params:
-                queryset = queryset.filter(
-                    user__enterprise__categories=params['category'])
-        return queryset
+
+        filter_location[0] = 'account__pincode__state__name'
+        filter_location[1] = 'account__pincode__city'
+        filter_location[2] = 'account__pincode__pincode'
+
+        query['user_type__in'] = ['subscriber']
+
+        if params.get('location'):
+            location_info = params.get('location', '').split(', ')
+
+            for index, value in enumerate(location_info):
+                query[filter_location.get(index)] = value
+
+        if params.get('category'):
+            query['subscriber_enterprise_user__categories__name'] = params.get(
+                'category', '').title()
+
+        try:
+            result = User.objects.filter(**query)
+        except Exception as e:
+            result = []
+        
+        return result
 
 
-class PincodeSearch(generics.ListAPIView):
-    queryset = Pincode.objects.all()
-    serializer_class = PincodeSerializer
-    filter_backends = (DjangoFilterBackend,)
-    filter_fields = ('pincode', 'state', 'city')
+class PincodeSearch(APIView):
+
+    def get(self, request, version, format=None):
+
+        data = []
+        EMPTY_RESPONSE = {'detail': 'Please pass a text parameter.'}
+        status_code = status.HTTP_400_BAD_REQUEST
+        text = request.query_params.get('text')
+
+        try:
+
+            if text:
+
+                text = str(text).title()
+                res = Pincode.objects.filter(
+                    Q(pincode__contains=text) | Q(city__contains=text) |
+                    Q(state__name__contains=text))[:50]
+
+                if res:
+                    data = PincodeSerializer(res, many=True).data
+                    data = self.format_location_data(data, text)
+
+                return Response(data, status=status.HTTP_200_OK)
+
+            else:
+                return Response(EMPTY_RESPONSE, status=status_code)
+
+        except Exception as e:
+            ERROR_RESPONSE = {'message': e}
+            return Response(ERROR_RESPONSE, status=status_code)
+
+    def format_location_data(self, data, text):
+
+        location_list = set()
+
+        for each_location in data:
+            location_string_list = list()
+
+            state = each_location.get('state') or ''
+            city = each_location.get('city') or ''
+            pincode = each_location.get('pincode') or ''
+
+            if text in state:
+                location_string_list.append(state)
+
+            if text in city:
+                # location_string_list.append(state)
+                location_string_list.append(city)
+
+            if text in pincode:
+                location_string_list.append(state)
+                location_string_list.append(city)
+                location_string_list.append(pincode)
+
+            location_string_list = self.remove_duplicates(location_string_list)
+
+            location_item = ', '.join(location_string_list)
+            location_list.add(location_item)
+
+
+        return location_list
+
+    @staticmethod
+    def remove_duplicates(location_string_list):
+        cleaned_list = []
+
+        for loc in location_string_list:
+            if loc not in cleaned_list:
+                cleaned_list.append(loc)
+
+        return cleaned_list
