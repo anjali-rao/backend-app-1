aaff3e8ebda35c00202925bde013e903825cf1fc
samim <samim@goplannr.com>
2019-04-09 17:13:11 +0530

    Rectified content/models.py.

--- content/models.py
+++ content/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from utils.models import BaseModel, models
+from utils.model import BaseModel, models
 from utils import get_choices, constants
 
 from product.models import Category
@@ -14,13 +14,15 @@ class Faq(BaseModel):
 
 class HelpFile(BaseModel):
     title = models.CharField(blank=True, max_length=512)
-    file = models.FileField(
-        upload_to=constants.HELP_FILES_PATH)
+    file = models.FileField(upload_to=constants.HELP_FILES_PATH)
     file_type = models.CharField(
         choices=get_choices(constants.HELP_FILES_TYPE),
         default="ALL", max_length=128
     )
-    category = models.CharField(max_length=512)
+    category = models.CharField(
+        choices=get_choices(
+            Category.objects.values_list('name', flat=True)
+        ), default="ALL", max_length=512)
 
 
 class ContactUs(BaseModel):
