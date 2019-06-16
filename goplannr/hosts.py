b753e95c533a399de34da2a3b0af13ef451fce60
hriks <hriks@outlook.com>
2019-06-16 20:28:06 +0530

    Swagger changes

--- goplannr/hosts.py
+++ goplannr/hosts.py
@@ -4,7 +4,6 @@ from django_hosts import patterns, host
 host_patterns = patterns(
     '',
     host(r'api', 'goplannr.apis_urls', name='api'),
-    host(r'swagger', 'goplannr.swagger_url', name='swagger'),
     host(r'payment', 'aggregator.urls', name='payment'),
     host(r'admin', settings.ROOT_URLCONF, name='admin'),
     host(r'www', settings.ROOT_URLCONF, name='www')
--- goplannr/swagger_url.py
+++
@@ -1,8 +0,0 @@
-from rest_framework_swagger.views import get_swagger_view
-from django.urls import re_path
-
-schema_view = get_swagger_view(title='Pastebin API')
-
-urlpatterns = [
-    re_path(r'^$', schema_view)
-]
