5ef305887d93c8823a9acdc7c200a542f546e678
hriks <hriks@outlook.com>
2019-06-21 17:56:46 +0530

    DateRangeExtended Filters

--- goplannr/settings.py
+++ goplannr/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'extended_filters',
     'rest_framework',
     'corsheaders',
     'django_hosts',
