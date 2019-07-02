38cd76a80f743c7f51e6dee94faa874c9afab407
anjali-rao <raoanjali0211@gmail.com>
2019-07-02 08:32:16 +0530

    sales journey testcases

--- manage.py
+++ manage.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         raise
     if 'send_report' in sys.argv:
         import requests
-        with open('.circleci/test_report.log', 'rb') as report:
+        with open('.circleci/test_report.log', 'r') as report:
             requests.post(
                 "https://hooks.slack.com/services/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", # noqa
                 json=dict(text=report.read()))
