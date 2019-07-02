f2ce7b77928592dd313623da69c21b0379908b53
hriks <hriks@outlook.com>
2019-07-02 13:02:19 +0530

    Create policy after receiving response from client

--- sales/models.py
+++ sales/models.py
@@ -113,6 +113,11 @@ class Application(BaseModel):
             current = self.__class__.objects.get(pk=self.id)
             if current.terms_and_conditions != self.terms_and_conditions and self.terms_and_conditions: # noqa
                 self.status = 'submitted'
+            if self.payment_failed != current.payment_failed and self.payment_mode != 'offline' and not self.payment_failed: # noqa
+                self.create_client()
+                earning = self.commission_set.get().earning
+                earning.status = 'application_submitted'
+                earning.save()
         except self.__class__.DoesNotExist:
             self.generate_reference_no()
             self.application_type = self.company_category.category.name.lower(
